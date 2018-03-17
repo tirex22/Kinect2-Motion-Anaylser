2d788e3a7ef7640f1fa59eac8789a2896427305b
tirex22 <youssefmaged95@gmail.com>
2018-03-17 13:49:10 +0200

    Add save loading

--- src/components/pages/KinectPreviewPage/SaveModal.js
+++ src/components/pages/KinectPreviewPage/SaveModal.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Modal, notification } from 'antd';
+import { Row, Modal, notification, Icon } from 'antd';
 import { getAllDatasets, createMotionModel } from '../../../firebase/firestore';
 import './KinectPreviewPage.css';
 
+var content;
 
 class SaveModal extends Component {
 
@@ -11,6 +12,7 @@ class SaveModal extends Component {
         this.state = {
             visible: false,
             datasets: [],
+            isSaving: false,
         }
 
         this.requestSave = this.requestSave.bind(this);
@@ -30,25 +32,57 @@ class SaveModal extends Component {
     }
 
     saveToDataset = (dataset) => {
-        createMotionModel(dataset.id, this.state.motionModel, (res) => {
-            if (res.success) {
-                notification['success']({
-                    message: 'Motion Model Successfully Add To ' + dataset.data.name,
-                    duration: 3
-                });
-                this.setState({ visible: false });
-                this.props.deleteMotion();
-            } else {
-                console.log(res.msg);
+        this.setState({ isSaving: true });
+
+        setTimeout(() => {
+            if (this.state.isSaving) {
+                this.setState({ isSaving: false });
                 notification['error']({
                     message: 'Failed To  Add Motion Model To ' + dataset.data.name,
                     duration: 3
                 });
             }
+        }, 30000);
+
+        createMotionModel(dataset.id, this.state.motionModel, (res) => {
+            if (res.success) {
+                if (this.state.isSaving) {
+                    notification['success']({
+                        message: 'Motion Model Successfully Add To ' + dataset.data.name,
+                        duration: 3
+                    });
+                    this.setState({ visible: false });
+                    this.props.deleteMotion();
+                }
+            } else {
+                if (this.state.isSaving) {
+                    console.log(res.msg);
+                    notification['error']({
+                        message: 'Failed To  Add Motion Model To ' + dataset.data.name,
+                        duration: 3
+                    });
+                }
+            }
+            this.setState({ isSaving: false });
         });
     }
 
     render() {
+
+        if (this.state.isSaving) {
+            content = (
+                <div>
+                    <Icon style={{ marginTop: 35 }} type="loading" className="loader" spin />
+                    <p className="not-found-error" >Saving Motion Model</p>
+                </div>
+            )
+        } else {
+            content = (
+                this.state.datasets.map((item, index) => (
+                    <a key={index} onClick={() => this.saveToDataset(item)} ><div className="save-button" >{item.data.name}</div></a>
+                )))
+        }
+
         return (
             <Modal className="save-modal"
                 visible={this.state.visible}
@@ -56,9 +90,7 @@ class SaveModal extends Component {
                 onCancel={() => this.setState({ visible: false })}
             >
                 <Row type="flex" justify="center" className="datasets-row" >
-                    {this.state.datasets.map((item, index) => (
-                        <a key={index} onClick={() => this.saveToDataset(item)} ><div className="save-button" >{item.data.name}</div></a>
-                    ))}
+                    {content}
                 </Row>
 
             </Modal>
